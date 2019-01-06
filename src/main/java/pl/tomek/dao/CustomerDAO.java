e3c341117348f8e10ae20bb151602f742ef2a374
TomaszSut <automek123@gmail.com>
2019-01-06 11:39:07 +0100

    sample

---
+++ src/main/java/pl/tomek/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package pl.tomek.dao;
+
+import java.util.List;
+
+import pl.tomek.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<%@page contentType="text/html" pageEncoding="UTF-8" isELIgnored="false"%>
+
+<html>
+<body>
+<h2>Spring CRM REST Demo </h2>
+<hr>
+<a href="${pageContext.request.contextPath}/api/customers">Get All Customers</a>
+</body>
+</html>
