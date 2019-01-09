77dc49237f35cf4694bbe36edf7abe1e690d0c01
TomaszSut <automek123@gmail.com>
2019-01-09 19:36:07 +0100

    security

---
+++ src/main/java/pl/tomek/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package pl.tomek.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
